a4d2a338a3294858447ec70aec601e95332f704f
adam <adam@harpur.io>
2019-10-26 17:21:17 +0100

    feat: added additional pages

---
+++ components/PostContent.vue
@@ -0,0 +1,27 @@
+  <template>
+    <div v-if="attributes" :key="$route.params.post">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="blog column is-10-tablet">
+          <div class="title">{{ attributes.title }}</div>
+          <div class="subtitle">
+            Published on {{ attributes.ctime }} by {{ attributes.author }}
+          </div>
+          <div class="blog-content content" v-html="content"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </template>
+  
+  <script>
+  export default {
+      props: {
+          attributes: {type: Object, required: true}
+      }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
--- pages/design/_posts/index.vue
+++ pages/design/_posts/index.vue
@@ -1,17 +1,5 @@
 <template>
-  <div v-if="attributes" :key="$route.params.post">
-    <div class="container">
-      <div class="columns is-centered">
-        <div class="blog column is-10-tablet">
-          <div class="title">{{ attributes.title }}</div>
-          <div class="subtitle">
-            Published on {{ attributes.ctime }} by {{ attributes.author }}
-          </div>
-          <div class="blog-content content" v-html="content"></div>
-        </div>
-      </div>
-    </div>
-  </div>
+<post-content></post-content>
 </template>
 <script>
 const fm = require('front-matter');
@@ -20,10 +8,16 @@ const md = require('markdown-it')({
   typographer: true
 });
 
+const fm = require('front-matter');
+const md = require('markdown-it')({
+  html: true,
+  typographer: true
+});
 export default {
   async asyncData({ params }) {
-    if (typeof params.posts === 'undefined') {
-      return { content: [], attributes: false };
+    if(typeof params.posts === "undefined"){
+      console.log("PARAMS!", params)
+      return {content: [], attributes: false}
     }
     // We read the markdown file by looking at the `post` parameter
     // in the URL and searching for a markdown file with that name in
